f7f7596be67625aad28d7d51d29307e07cbc61c2
Vesylum <thevesylum@gmail.com>
2025-06-08 18:51:52 -0400

    fix storageGet clone default profiles

---
+++ tests/defaultProfilesClone.test.js
@@ -0,0 +1,45 @@
+const { JSDOM } = require('jsdom');
+const QUnit = require('qunit');
+
+let $;
+
+QUnit.module('defaultProfiles clone', hooks => {
+  hooks.beforeEach(() => {
+    const dom = new JSDOM('<!doctype html><html><body>' +
+      '<input type="checkbox" id="foo_1_1">' +
+      '</body></html>', { url: 'http://localhost' });
+    const { window } = dom;
+    global.window = window;
+    global.document = window.document;
+
+    delete require.cache[require.resolve('jquery')];
+    $ = require('jquery');
+    global.$ = global.jQuery = $;
+    document.dispatchEvent(new window.Event('DOMContentLoaded'));
+
+    $.getJSON = (_url, cb) => { setTimeout(() => cb({}), 0); };
+
+    delete require.cache[require.resolve('../js/main.js')];
+    require('../js/main.js');
+    return new Promise(r => setTimeout(r, 50));
+  });
+
+  hooks.afterEach(() => {
+    delete global.window;
+    delete global.document;
+    delete global.$;
+  });
+
+  QUnit.test('resetProgress should clear stored checklist data', assert => {
+    const checkbox = document.getElementById('foo_1_1');
+    $(checkbox).trigger('click');
+
+    let store = JSON.parse(window.localStorage.getItem('profiles'));
+    assert.ok(store.profiles['Default Profile'].checklistData['foo_1_1'], 'progress saved');
+
+    window.resetProgress();
+
+    store = JSON.parse(window.localStorage.getItem('profiles'));
+    assert.deepEqual(store.profiles['Default Profile'].checklistData, {}, 'checklist cleared');
+  });
+});
